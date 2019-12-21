6b91d66f9650a53532aeda82d413a589a12a1441
karta22121995 <57282743+karta22121995@users.noreply.github.com>
2019-12-21 21:01:53 +0800

    完成攝影機追蹤

---
+++ UnityProject/Assets/Scripts/LearnLerp.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class LearnLerp : MonoBehaviour
+{
+    private Vector3 a = new Vector3(0, 0, 0);
+    private Vector3 b = new Vector3(10, 10, 10);
+
+    public Color red = Color.red;
+    public Color blue = Color.blue;
+    public Color newColor;
+
+    private void Start()
+    {
+        // 認識差值 Lerp
+        print(Mathf.Lerp(0, 10, 0.5f));
+        print(Vector3.Lerp(a, b, 0.5f));
+        newColor = Color.Lerp(red, blue, 0.5f);
+    }
+
+}
---
+++ UnityProject/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [Header("是否亦開始顯示隨機技能")]
+    public bool showRandomSkill;
+    [Header("是否自動開門")]
+    public bool autoOpenDoor;
+    [Header("隨機技能介面")]
+    public GameObject randomSkill;
+
+    private Animator door;
+
+    /// <summary>
+    /// 顯示隨機技能介面
+    /// </summary>
+    private void Start()
+    {
+        door = GameObject.Find("門").GetComponent<Animator>();
+
+        if (autoOpenDoor) Invoke("OpenDoor", 6);       // 延遲調用("方法名稱"，延遲時間)
+        if (showRandomSkill) ShowRandomSkill();
+    }
+
+    private void ShowRandomSkill()
+    {
+        randomSkill.SetActive(true);
+    }
+
+    private void OpenDoor()
+    {
+        door.SetTrigger("開門");     // 動畫控制器.設定觸發("參數名稱")
+    }
+}
